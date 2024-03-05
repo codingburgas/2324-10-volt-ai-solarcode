5c0c71ad2db37906bf0a0b91e1b87ec31e9065fe
Lazar <LPApostolov21@codingburgas.bg>
2024-03-05 18:38:09 +0200

    Add the dummy data base

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 """
 The main module of the application. Connects the builder, the UI, and the data.
 """
+
+#I am not so sure if i made the dummy write
+
+from faker import Faker
+import pandas as pd
+fake = Faker()
+data = [fake.profile() for i in range(50)]
+data = pd.DataFrame(data)
+print(data.head())
